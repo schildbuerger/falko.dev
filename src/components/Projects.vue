b2d0a0c792f5d28f836c6e0483ce5d5c96c43545
Falko Möller <falko@Falkos-MBP.fritz.box>
2019-07-22 09:54:54 +0200

    first project draft

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -2,17 +2,32 @@
   <section class="section is-fullheight hero">
     <div class="hero-body">
       <div class="container is-vcentered">
-        <h2 class="title is-2">Product Contributions</h2>
         <div class="columns">
-          <div class="column is-one-fifth">
-            <div class="card">
-              <div class="card-image">
-                <figure class="image">
-                  <img
-                    src="https://bulma.io/images/placeholders/128x128.png"
-                    alt="Placeholder image"
-                  />
-                </figure>
+          <div class="column has-background-olaph box">
+            <div class="columns is-vcentered">
+              <div class="column is-one-half is-offset-1">
+                <div class="content">
+                  <h3 class="title has-text-white is-size-1">Olaph.io</h3>
+                  <p class="subtitle has-text-white is-size-7 is-uppercase">
+                    Product Lead
+                  </p>
+                  <p class="has-text-white is-size-6">
+                    A Slack Standup Bot
+                  </p>
+                  <a class="button is-white">More</a>
+                </div>
+              </div>
+              <div class="column is-one-half">
+                <div class="">
+                  <div class="card-image">
+                    <figure class="image">
+                      <img
+                        src="../assets/logo_olaph.svg"
+                        alt="Placeholder image"
+                      />
+                    </figure>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -31,4 +46,8 @@ export default {
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.has-background-olaph {
+  background-color: #31b5aa !important;
+}
+</style>
