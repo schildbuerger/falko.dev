f727629d121e77bcc6e380e576791fa7ac74595c
Falko Möller <falko@Falkos-MBP.fritz.box>
2019-09-04 13:02:55 +0200

    update

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,26 +1,18 @@
 <template>
-  <section class="section is-fullheight hero bg-olaph">
-    <div class="columns is-fullheight hero-body">
-      <div class="column is-4 is-offset-1">
-        <div class="content">
-          <h3 class="title has-text-white is-size-1">Olaph</h3>
-          <p class="subtitle has-text-white is-size-6">
-            A Slack bot for daily standups.
-          </p>
-          <a class="button  is-outlined is-white is-medium m-t-sm">
-            <span>Show Details</span>
-            <span class="icon is-small">
-              <i class="fas fa-caret-right"></i> </span
-          ></a>
+  <section class="section is-fullheight hero">
+    <div class="container is-vcentered">
+      <div class="columns">
+        <div class="column is-2 is-offset-1 has-background-primary">
+          First column
         </div>
-      </div>
-      <div class="column is-6 level">
-        <div class="level-item has-text-centered">
-          <div class="card-image">
-            <figure class="image m-t-xl">
-              <img src="../assets/olaph_preview.png" alt="Placeholder image" />
-            </figure>
-          </div>
+        <div class="column is-2 is-offset-1 has-background-primary">
+          Second column
+        </div>
+        <div class="column is-2 is-offset-1 has-background-primary">
+          Third column
+        </div>
+        <div class="column is-2 is-offset-1 has-background-primary">
+          Fourth column
         </div>
       </div>
     </div>
@@ -37,14 +29,7 @@ export default {
 </script>
 
 <style lang="scss">
-.bg-olaph {
-  /* background: rgb(2, 0, 36);
-  background: linear-gradient(
-    90deg,
-    #31b5aa 0%,
-    #73c4bc 35%,
-    #d0e9e5 150%
-  ) !important;*/
+.bg- {
   background-color: #31b5aa !important;
   background: url("https://i.pinimg.com/originals/e9/00/5c/e9005c830342a839b6e259be4b0c02ed.jpg")
     center center no-repeat fixed;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="has-scroll-snap">
     <cHero />
+    <cProject />
     <cProjectCollection />
     <cBrands />
     <cContact />
@@ -13,6 +14,7 @@ import cHero from "@/components/Hero.vue";
 import cProjectCollection from "@/components/ProjectCollection.vue";
 import cBrands from "@/components/Brands.vue";
 import cContact from "@/components/Contact.vue";
+import cProject from "@/components/Project.vue";
 
 export default {
   name: "vHome",
@@ -20,7 +22,8 @@ export default {
     cHero,
     cProjectCollection,
     cBrands,
-    cContact
+    cContact,
+    cProject
   }
 };
 </script>
