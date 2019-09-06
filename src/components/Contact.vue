ff912c384da1474575189080093a6fca6992186a
Falko Möller <falko@Falkos-MBP.fritz.box>
2019-09-06 17:05:31 +0200

    update

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,49 +1,51 @@
 <template>
-  <section class="section is-fullheight hero bg-berlin">
-    <div class="hero-body m-b-lg">
-      <div class="container is-vcentered">
-        <div class="columns">
-          <div
-            class="column is-4-desktop is-offset-4 is-6-tablet is-offset-3-tablet is-12-mobile card"
-          >
-            <div class="m-b-md has-text-centered card-content">
-              <h3 class="title is-1">Contact</h3>
-              <span class="icon">
-                <i class="fas fa-2x fa-envelope"></i>
-              </span>
-              <h4 class="title is-4 m-t-sm">
-                hi@falko.fyi
-              </h4>
-              <div class="columns is-mobile">
-                <div class="column is-2 is-offset-3 has-text-centered">
-                  <a
-                    class="icon is-large has-text-dark"
-                    href="https://www.linkedin.com/in/falkomoeller/"
-                    target="_blank"
-                  >
-                    <i class="fab fa-2x fa-linkedin-in"></i>
-                  </a>
-                </div>
-                <div class="column is-2 has-text-centered">
-                  <a
-                    class="icon is-large has-text-dark"
-                    href="https://twitter.com/prod_ct"
-                    target="_blank"
-                  >
-                    <i class="fab fa-2x fa-twitter"></i>
-                  </a>
-                </div>
-                <div class="column is-2 has-text-centered">
-                  <a
-                    class="icon is-large has-text-dark"
-                    href="https://medium.com/@falkomoeller"
-                    target="_blank"
-                  >
-                    <i class="fab fa-2x fa-medium"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
+  <section class="section is-fullheight hero bg is-hidden-mobile">
+    <div class="columns is-fullheight hero-body is-hidden-mobile">
+      <div
+        class="column is-4-desktop is-offset-1-desktop is-12-mobile is-5-tablet is-offset-1-tablet"
+      >
+        <div class="content">
+          <h3 class="title is-1 has-text-white">
+            That's all folks!<br />
+            <span class="title is-1 has-text-grey-light">
+              For more please contact me.
+            </span>
+          </h3>
+        </div>
+      </div>
+      <div
+        class="column is-4-desktop is-offset-1-desktop is-12-mobile is-5-tablet is-offset-1-tablet has-text-centered"
+      >
+        <h4 class="title is-1 has-text-white">
+          hi@falko.fyi
+        </h4>
+        <div class="columns is-mobile">
+          <div class="column is-2 is-offset-3 has-text-centered">
+            <a
+              class="icon is-large has-text-white"
+              href="https://www.linkedin.com/in/falkomoeller/"
+              target="_blank"
+            >
+              <i class="fab fa-2x fa-linkedin-in"></i>
+            </a>
+          </div>
+          <div class="column is-2 has-text-centered">
+            <a
+              class="icon is-large has-text-white"
+              href="https://twitter.com/prod_ct"
+              target="_blank"
+            >
+              <i class="fab fa-2x fa-twitter"></i>
+            </a>
+          </div>
+          <div class="column is-2 has-text-centered">
+            <a
+              class="icon is-large has-text-white"
+              href="https://medium.com/@falkomoeller"
+              target="_blank"
+            >
+              <i class="fab fa-2x fa-medium"></i>
+            </a>
           </div>
         </div>
       </div>
@@ -61,9 +63,10 @@ export default {
 };
 </script>
 
-<style lang="scss">
-.bg-berlin {
-  background: url("") center center no-repeat fixed;
+<style lang="scss" scoped>
+.bg {
+  background: url(/img/bg01.d9aa08fb.jpg) center left 30vh no-repeat fixed;
   background-size: cover;
+  background-color: #2d2e44;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="has-scroll-snap">
     <cHero />
-    <cProject />
     <cProjectCollection />
     <cBrands />
     <cContact />
@@ -14,7 +13,6 @@ import cHero from "@/components/Hero.vue";
 import cProjectCollection from "@/components/ProjectCollection.vue";
 import cBrands from "@/components/Brands.vue";
 import cContact from "@/components/Contact.vue";
-import cProject from "@/components/Project.vue";
 
 export default {
   name: "vHome",
@@ -22,8 +20,7 @@ export default {
     cHero,
     cProjectCollection,
     cBrands,
-    cContact,
-    cProject
+    cContact
   }
 };
 </script>
