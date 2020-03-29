c9688540666ea99b7e1491446a0bce74b603af63
Falko <falko.moeller@gmail.com>
2020-03-29 14:15:44 +0200

    update

---
+++ src/components/Cards.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="section is-fullheight hero bg">
+    <div class="hero-body">
+      <div class="container has-text-right is-vcentered">
+        <h1 class="title">Cards</h1>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "cCards",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped lang="sass">
+.bg
+  background: url("") center center no-repeat fixed
+  background-size: cover
+  @include tablet
+    background-color: white
+</style>
